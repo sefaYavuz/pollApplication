f5179efde0449a5c356d937ae90ac1105cf2d855
Sefa Yavuz <seff61@gmail.com>
2014-07-08 16:29:56 +0200

    added delete handler

--- app/controllers/QuestionController.php
+++ app/controllers/QuestionController.php
@@ -20,5 +20,28 @@ class QuestionController extends \Phalcon\Mvc\Controller
     	}
     }
 
+    public function deleteAction($questionId)
+    {
+        $question = Questions::findFirstById($questionId);
+
+        $option = Options::findByQuestion_id($questionId);
+
+        if($question->delete() == false || $option->delete() == false)
+        {
+            echo 'Oeps, er is iets fouts gegaan: <br />';
+
+            foreach ($option->getMessages() as $message) 
+            {
+                echo $message, "<br />";
+            }
+        }
+        else
+        {
+            echo '<p>De vraag is met succes verwijderd!</p>';
+
+            return $this->response->redirect('');
+        }
+    }
+
 }
 
